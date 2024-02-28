8eff724d47488b7c6f7951829ab7bdd1ffc23d1c
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2024-02-28 16:32:36 -0300

    ajustes de erros de dependencia

---
+++ frontend/src/main/App.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+export default props =>
+    <BrowserRouter>
+        <div className="app">
+            
+        </div>
+    </BrowserRouter>
