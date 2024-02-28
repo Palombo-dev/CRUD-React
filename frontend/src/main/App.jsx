ccd0fb94510c3987bfde3a94642fea0f60b3c31a
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2024-02-28 16:47:57 -0300

    Fix erros em app.jsx

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,11 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
+import Footer from '../components/template/Footer'
 
 export default props =>
-    <BrowserRouter>
-        <div className="app">
-            
-        </div>
-    </BrowserRouter>
+    <div className="app">
+        <Logo />
+        <Nav />
+        <Main />
+        <Footer />
+    </div>
