729206493390c4e706662e99873837e4c3d39d57
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2024-03-06 18:31:42 -0300

    feature: implementando react router

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -2,10 +2,11 @@ import './Logo.css'
 import logo from '../../assets/imgs/react-logo.png'
 import React from 'react'
 import '../../main/App.css'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside ClassName="logo">
-        <a href="/" className="logo">
+        <Link to="/" className="logo">
             <img src={logo} alt="logo" />
-        </a>
+        </Link>
     </aside>
