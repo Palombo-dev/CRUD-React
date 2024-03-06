7089cdd9c25a7265413fe8eb93603711533d966e
Rodrigo Palombo <116583410+Palombo-dev@users.noreply.github.com>
2024-03-06 18:58:39 -0300

    feature: preparando o crud

--- frontend/src/components/user/userCrud.jsx
+++ frontend/src/components/user/userCrud.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import Main from '../template/Main'
 
 const headerProps = {
@@ -6,7 +7,36 @@ const headerProps = {
     title: 'Usuários',
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
 }
+
+const baseURL = 'http://localhost:3000/users'
+const initialState = {
+    user: {name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends Component {
+    state = { ...initialState }
+
+    clear(){
+        this.setState({ user: initialState.user})
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseURL}/${user.id}` : baseURL
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdateList(resp.data)
+                this.setState({ user: initialState.user, list })
+            })
+    }
+
+    getUpdateList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
     render() {
         return (
             <Main {...headerProps}>
